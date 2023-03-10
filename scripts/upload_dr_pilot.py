f3ef8b60b22d3bbb4c059f0f804fddbcd953fac8
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-03-10 12:12:33 -0800

    Update pilot scriprt

--- scripts/upload_dr_pilot.py
+++ scripts/upload_dr_pilot.py
@@ -1,5 +1,4 @@
 import pathlib
-import logging
 
 import np_logging
 import np_datajoint
@@ -8,18 +7,24 @@ from np_datajoint.comparisons import *
 from np_datajoint.config import *
 from np_datajoint.utils import *
 
-logger = logging.getLogger('root')
-logger.handlers = []
 logger = np_logging.getLogger()
-
+logger.setLevel('INFO')
+np_logging.getLogger('botocore').setLevel(logging.WARNING)
 RUNNING_ON_HPC = True # prevent transfer of job to HPC - won't work with DRPilot class currently
 
-for root in DRPilot.storage_dirs:
-    for path in root.glob('DRpilot*'):
-        session = DRPilot(path)
-        probe_dirs = tuple(session.path.glob('*_probe*'))
-        if not probe_dirs:
-            print(f'No probe dirs found for {session.path.name}')
-            continue
+while True:
+    with contextlib.suppress(Exception):
+        for root in DRPilot.storage_dirs[1:]:
+            for path in root.glob('DRpilot*'):
+                session = DRPilot(path)
+                probe_dirs = tuple(session.path.glob('*_probe*'))
+                probes = ''.join(_ for _ in DEFAULT_PROBES for d in probe_dirs if next(d.rglob(f'*.Probe{_}-*'), None))
+                
+                if not probe_dirs:
+                    logger.info(f'No probe dirs found for {session.path.name}')
+                    continue
+                logger.info(f'Uploading {session.path.name}, probes {probes}...')
 
-        session.upload(probe_dirs)
+                session.upload(probe_dirs, probes)
+        logger.info(f'Finished uploading all sessions')
+        break
